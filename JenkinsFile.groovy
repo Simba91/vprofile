c215f34683da54cd1e4ae468fb933968fa8b26b7
Hoai Bao <thbao91@gmail.com>
2023-01-16 22:24:34 +0700

    testing jenkins file

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,11 +1,6 @@
 pipeline {
     agent any
     stages {
-        stage('Fetch code') {
-            steps {
-                git branch: testing-groovy-file, url: 'https://github.com/Simba91/vprofile.git'
-            }
-        }
         stage('Build') {
             steps {
                 sh 'mvn install'
